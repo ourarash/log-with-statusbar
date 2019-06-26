a581f76ea7dd9011ac7ad3d7f48ea272bbce28f7
Ari <ourarash@gmail.com>
2019-06-26 08:53:22 -0700

    First commit

---
+++ demo.js
@@ -0,0 +1,45 @@
+/**
+ * To run: 
+ *  npm install progress-string
+ *  node demo.js
+ */
+const log = require("./index.js");
+var progressString;
+try {
+   progressString = require("progress-string");
+} catch (error) {
+  console.log(`Please run "npm install progress-string" before running the demo.`);
+  process.exit();  
+}
+
+let i = 0;
+let maxCount = 20;
+
+var pBar = progressString({
+  width: 40,
+  total: maxCount,
+  complete: "█",
+  incomplete: "░",
+  style: function(complete, incomplete) {
+    return complete + "▒" + incomplete ;
+  }
+});
+
+setInterval(() => {
+  var date = new Date();
+  log(date);
+  log.info(`Hi this is log line ${i++}`);
+  log.info(`Here is another log line ${i}`);
+  log.info(`-----------------------------------------------------------------`);
+  if(i<5){
+    log.setStatusBarText([`Complete!`]);
+  } else{
+    log.setStatusBarText([`Progress: ${pBar(i)}`]);
+
+  }
+  if (i == maxCount) {
+    i = 0;
+    
+  }
+
+}, 100);
---
+++ index.js
@@ -0,0 +1,56 @@
+/**
+ * A light weight logger with a status bar on the bottom that does not disappear with scrolling
+ * The status bar can be multiple lines.
+ * Use setStatusBarText() function to set the lines.
+ * This is based on npm ololog package
+ * By: Ari Saif
+ */
+/**
+ * @module testModule
+ */
+const ansi = require("ansi"),
+  cursor = ansi(process.stdout),
+  ololog = require("ololog").configure({ locate: false }),
+  assert = require("assert");
+
+var g_statusTextArray = [
+  `Call log.setStatusBarText(["Your Text"]) to set this line.`
+];
+
+var log = ololog.configure({
+  "+render"(text) {
+    g_statusTextArray.forEach((l, i) => {
+      cursor.up();
+      cursor.eraseLine();
+    });
+    return text;
+  },
+  "render+"(text) {
+    g_statusTextArray.forEach(l => {
+      let k = String.raw`${l}`;
+      k = k.replace(/\n/, "\\n");
+      k = k.substring(0, process.stdout.columns);
+      console.log(k);
+    });
+    return text;
+  }
+});
+
+log.methods({
+  /**
+   *
+   * @param {Array} arrayOfStatusLines An array of values.
+   * Each value will be printed in a separate line.
+   * New lines will be escaped
+   * @returns {null}
+   */
+  setStatusBarText(arrayOfStatusLines) {
+    assert(
+      Array.isArray(arrayOfStatusLines),
+      "Error: The parameter to setStatusBarText should be an array."
+    );
+    g_statusTextArray = arrayOfStatusLines;
+  }
+});
+
+module.exports = log;
